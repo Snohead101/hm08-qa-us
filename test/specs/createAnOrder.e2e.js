6ffbd0b8b466e1935b3abeeeac69e855c05b36f9
Amaro Segovia <amarosegovia769@gmail.com>
2024-06-11 22:57:42 -0400

    project 8

--- test/specs/createAnOrder.e2e.js
+++ test/specs/createAnOrder.e2e.js
@@ -2,23 +2,6 @@ const page = require('../../page');
 const helper = require('../../helper')
 
 describe('task 1', () => {
-    //it('should open phone number modal', async () => {
-        // await browser.url(`/`)
-       // await page.fillAddresses('East 2nd Street, 601', '1300 1st St');
-       // const phoneNumberButton = await $(page.phoneNumberButton);
-        // await phoneNumberButton.waitForDisplayed();
-       // await phoneNumberButton.click();
-       // const pnoneNumberModal = await $(page.phoneNumberModal);
-       // await expect(pnoneNumberModal).toBeExisting();
-   // })
-
-    //it('should save the phone', async () => {
-        //await browser.url(`/`)
-        //await page.fillAddresses('East 2nd Street, 601', '1300 1st St');
-       // const phoneNumber = helper.getPhoneNumber("+1");
-       // await page.submitPhoneNumber(phoneNumber);
-       // await expect(await helper.getElementByText(phoneNumber)).toBeExisting();
-   // })
 
     it('setting the address', async () => {
         await browser.url(`/`)
@@ -123,7 +106,6 @@ describe('task 1', () => {
         await browser.pause({ pauseTime: 2000 });
         const iceScreamCounter = await $(page.iceScreamCounter).getText();
         await expect(iceScreamCounter).toBe("2");
-        //await expect(await $(page.iceScreamCounter).getText()).toBe("2");
 
     })
 
